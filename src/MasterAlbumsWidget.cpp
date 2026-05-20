a24e749d1ff657e53dacf1ef569e3d18420c7550
SERA LINA C. <lina.chikh@etu.univ-amu.fr>
2026-05-20 08:06:45 +0200

    Création classe MasterAlbumsWidget

---
+++ src/MasterAlbumsWidget.cpp
@@ -0,0 +1,10 @@
+#include "MasterAlbumsWidget.hpp"
+
+MasterAlbumsWidget::MasterAlbumsWidget(QWidget *parent): QWidget{parent}
+{}
+
+
+void MasterAlbumsWidget::onSelectionChanged(){};
+void MasterAlbumsWidget::onAddButtonClicked(){};
+void MasterAlbumsWidget::onRemoveButtonClicked(){};
+
