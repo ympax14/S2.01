ae564e5167df0c084384dafb6b19df791864dc90
SERA LINA C. <lina.chikh@etu.univ-amu.fr>
2026-06-08 01:54:58 +0200

    Ajout de traduction française avec QTLinguist

--- langs/SAE-S2_01_fr_FR.ts
+++ langs/SAE-S2_01_fr_FR.ts
@@ -1,3 +1,336 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS >
-<TS version="2.1" language="fr_FR"></TS>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>DetailsWidget</name>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="27"/>
+        <source>Personal notes...</source>
+        <translation>Notes Personnelles...</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="76"/>
+        <location filename="../src/DetailsWidget.cpp" line="215"/>
+        <source>Title:</source>
+        <translation>Titre:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="80"/>
+        <source>Image URL / file path...</source>
+        <translation>URL de l&apos;image / Chemin...</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="88"/>
+        <source>Browse...</source>
+        <translation>Parcourir...</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="91"/>
+        <source>Select image</source>
+        <translation>Séléctionner une image</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="92"/>
+        <source>Images (*.png *.jpg *.jpeg *.bmp *.gif)</source>
+        <translation>Images (*.png *.jpg *.jpeg *.bmp *.gif)</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="101"/>
+        <source>Image:</source>
+        <translation>Image:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="110"/>
+        <source>Artist:</source>
+        <translation>Artiste:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="119"/>
+        <source>Compositor:</source>
+        <translation>Compositeur:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="133"/>
+        <source>Category:</source>
+        <translation>Catégorie:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="144"/>
+        <source>Year:</source>
+        <translation>Année:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="157"/>
+        <source>Support:</source>
+        <translation>Support:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="161"/>
+        <source> €</source>
+        <translation> €</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="170"/>
+        <source>Price:</source>
+        <translation>Prix:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="172"/>
+        <source>Songs:</source>
+        <translation>Chansons:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="206"/>
+        <source>Index:</source>
+        <translation>Index:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="219"/>
+        <source> s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="227"/>
+        <source>Duration:</source>
+        <translation>Durée:</translation>
+    </message>
+    <message>
+        <location filename="../src/DetailsWidget.cpp" line="240"/>
+        <source>Rating:</source>
+        <translation>Note:</translation>
+    </message>
+</context>
+<context>
+    <name>MasterAlbumsWidget</name>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="6"/>
+        <source>Albums</source>
+        <translation>Albums</translation>
+    </message>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="10"/>
+        <source>➕ Album</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="11"/>
+        <source>➖ Album</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="12"/>
+        <source>💾 Album</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="13"/>
+        <source>🗁 Album</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="14"/>
+        <source>➕ Song</source>
+        <translation>➕ Chanson</translation>
+    </message>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="15"/>
+        <source>➖ Song</source>
+        <translation>➖ Chanson</translation>
+    </message>
+    <message>
+        <location filename="../src/MasterAlbumsWidget.cpp" line="27"/>
+        <source>Search ...</source>
+        <translation>Rechercher...</translation>
+    </message>
+</context>
+<context>
+    <name>NowPlayingWindow</name>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="15"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="18"/>
+        <source>&amp;Albums Collection</source>
+        <translation>&amp;Collection</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="19"/>
+        <source>&amp;Save As...</source>
+        <translation>&amp;Enregistrer Sous...</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="21"/>
+        <source>&amp;Add Album from file</source>
+        <translation>&amp;Ajouter Album à partir de fichier</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="22"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="23"/>
+        <source>&amp;Albums Collection (XML)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="24"/>
+        <source>&amp;Save As... (XML)</source>
+        <translation>&amp;Enregistrer sous...(XML)</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="96"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="259"/>
+        <source>Open AlbumsCollection</source>
+        <translation>Ouvrir la Collection</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="108"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="270"/>
+        <source>Collection Loaded</source>
+        <translation>Collection chargée</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="108"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="270"/>
+        <source>Collection loaded successfully</source>
+        <translation>Collection chargée avec succès</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="110"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="272"/>
+        <source>Error while loading the album&apos;s collection !</source>
+        <translation>Erreur lors du chargement de la collection !</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="110"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="272"/>
+        <source>Invalid collection file !</source>
+        <translation>Fichier de collection invalide !</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="128"/>
+        <source>New Song !</source>
+        <translation>Nouvelle chanson !</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="128"/>
+        <source>a new song has been added</source>
+        <translation>une nouvelle chanson a été ajoutée</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="136"/>
+        <source>Song Removed</source>
+        <translation>Chanson Supprimée</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="136"/>
+        <source> a song has been removed.</source>
+        <translation>Une chanson a été supprimée</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="145"/>
+        <source>New Album</source>
+        <translation>Nouvel Album</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="145"/>
+        <source>a new album has been added</source>
+        <translation>Un nouvel album a été ajouté</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="149"/>
+        <source>Save Album</source>
+        <translation>Sauvegarder Album</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="167"/>
+        <source>Removed</source>
+        <translation>Supprimé</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="167"/>
+        <source>Album removed successfully !</source>
+        <translation>Album supprimé</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="172"/>
+        <source>Open album</source>
+        <translation>Ouvrir Album</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="184"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="188"/>
+        <source>Error while loading Album !</source>
+        <translation>Erreur lors du chargement de l&apos;album !</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="184"/>
+        <source>This album is already loaded !</source>
+        <translation>Cet album est déjà chargé !</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="188"/>
+        <source>Invalid album file !</source>
+        <translation>Fichier album invalide !</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="198"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="281"/>
+        <source>Save AlbumsCollection</source>
+        <translation>Sauvegarder la collection</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="209"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="292"/>
+        <source>Saved</source>
+        <translation>Sauvegardé</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="209"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="292"/>
+        <source>Collection saved successfully :&gt;</source>
+        <translation>Collection sauvegardée :&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="212"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="294"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="312"/>
+        <source>Save failed !!</source>
+        <translation>La Sauvegarde a Echoué !!</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="212"/>
+        <source>One or more albums are invalid (probably missing title or artist?) </source>
+        <translation>Un ou plusieurs albums sont invalides(peut-être un titre ou artiste manquant ?)</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="217"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="300"/>
+        <source>Save Collection As</source>
+        <translation>Enregistrer la collection sous</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="228"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="309"/>
+        <source>Saved As</source>
+        <translation>Enregistré sous</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="228"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="309"/>
+        <source>Collection saved to new file successfully :&gt;</source>
+        <translation>Collection sauvegardée dans un nouveau fichier :&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="235"/>
+        <source>Unsaved changes</source>
+        <translation>Modifications non sauvegardées</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="236"/>
+        <source>You have unsaved changes. Do you perhaps want to save before quitting ?</source>
+        <translation>Vous avez des modifications non sauvegardées. Voulez-vous sauvegarder avant de quitter ?</translation>
+    </message>
+    <message>
+        <location filename="../src/NowPlayingWindow.cpp" line="294"/>
+        <location filename="../src/NowPlayingWindow.cpp" line="312"/>
+        <source>One or more albums are invalid !</source>
+        <translation>Un ou plusieurs albums sont invalides !</translation>
+    </message>
+</context>
+</TS>
